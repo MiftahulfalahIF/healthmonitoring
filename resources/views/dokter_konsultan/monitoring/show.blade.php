1f10e6d3759a9cbe2d7bddf642fb0cde60800a5b
Gerry Herdianto <gerry.herdianto@gmail.com>
2019-06-13 15:47:54 +0700

    ok

--- resources/views/dokter_konsultan/monitoring/show.blade.php
+++ resources/views/dokter_konsultan/monitoring/show.blade.php
@@ -1,57 +1,82 @@
 @extends('layout')
 
 @section('body')
-<div class="container-fluid">
-@if(Session::has('msg'))
-        <div class="alert alert-success mt-3">
-          {{ session('msg') }}
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+
+
+<section class="content-header">
+    <h1>
+        Monitoring
+    </h1>
+</section>
+
+<section class="content">
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Detail Monitoring</h3>
+            <div class="box-tools">
+                
+            </div>
         </div>
-        @endif
-        
-        <div class="panel">
-        	<div class="panel-body">
-        		<table>
-        			<tr>
-        				<td width="200">Nama Pasien</td>
-        				<td>: {{ $monitoring->pasien->nama }}</td>
-        			</tr>
-        			<tr>
-        				<td>Nomor Monitoring</td>
-        				<td>: {{ $monitoring->no_monitoring }}</td>
-        			</tr>
-        			<tr>
-        				<td>Nama Dokter Konsultan</td>
-        				<td>: {{ $monitoring->dokter_konsultan->nama }}</td>
-        			</tr>
-        			<tr>
-        				<td>Klinik Awal</td>
-        				<td>: {{ $monitoring->klinik_awal}}</td>
-        			</tr>
-        			<tr>
-        				<td>Tanggal Dimulai</td>
-        				<td>: {{ $monitoring->tgl_mulai}}</td>
-        			</tr>
-        			<tr>
-        				<td>Tahap Pengobatan</td>
-        				<td>: {{ $monitoring->tahap_pengobatan}}</td>
-        			</tr>
-        			<tr>
-        				<td>Kontrol Yang Harus Dilakukan</td>
-        				<td>: {{ $monitoring->jml_kontrol}}</td>
-        			</tr>
-        			<tr>
-        				<td>Status Monitoring</td>
-        				<td>: {{ $monitoring->status}}</td>
-        			</tr>
-        		</table>
-                <div class="card-body">
-                     <a href="{{ action('DokterKonsultan\KontrolController@create', [$monitoring->id]) }}" class="btn btn-primary">Tambah Kontrol</a>
-                </div>
-        
-                <table class="table table-bordered">
+
+        <div class="box-body">
+
+            @if(Session::has('msg'))
+            <div class="alert alert-success mt-3">
+                {{ session('msg') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @endif
+
+            <table>
+                <tr>
+                    <td width="200">Nama Pasien</td>
+                    <td>: {{ $monitoring->pasien->nama }}</td>
+                </tr>
+                <tr>
+                    <td>Nomor Monitoring</td>
+                    <td>: {{ $monitoring->no_monitoring }}</td>
+                </tr>
+                <tr>
+                    <td>Nama Dokter Konsultan</td>
+                    <td>: {{ $monitoring->dokter_konsultan->nama }}</td>
+                </tr>
+                <tr>
+                    <td>Klinik Awal</td>
+                    <td>: {{ $monitoring->klinik_awal}}</td>
+                </tr>
+                <tr>
+                    <td>Tanggal Dimulai</td>
+                    <td>: {{ $monitoring->tgl_mulai}}</td>
+                </tr>
+                <tr>
+                    <td>Tahap Pengobatan</td>
+                    <td>: {{ $monitoring->tahap_pengobatan}}</td>
+                </tr>
+                <tr>
+                    <td>Kontrol Yang Harus Dilakukan</td>
+                    <td>: {{ $monitoring->jml_kontrol}}</td>
+                </tr>
+                <tr>
+                    <td>Status Monitoring</td>
+                    <td>: {{ $monitoring->status}}</td>
+                </tr>
+            </table>
+
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Tabel Kontrol</h3>
+            <div class="box-tools">
+                <a href="{{ action('DokterKonsultan\KontrolController@create', [$monitoring->id]) }}" class="btn btn-primary">Tambah Kontrol</a>
+            </div>
+        </div>
+
+        <div class="box-body">
+            <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th>No Kontrol</th>
@@ -59,16 +84,14 @@
                         <th>Tanggal Kontrol</th>
                         <th width="60">Aksi</th>
                     </tr>
-                    </thead>
-               
-                
+                </thead>
+                <tbody>
+                    
                 </tbody>
-                </table>
-        	</div>
+            </table>
         </div>
-
-	</div>
-
+    </div>
+</section>
 
 
 
